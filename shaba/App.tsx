643156908e222c1694a78423d4f323aa4e9689e3
JoahanOrtega <joahangab23@gmail.com>
2024-05-06 10:26:20 -0600

    fix folders structure

--- shaba/App.tsx
+++ shaba/App.tsx
@@ -1,30 +1,30 @@
-import { config } from '@gluestack-ui/config';
-import { Box, GluestackUIProvider, Text } from '@gluestack-ui/themed';
-import { ScrollView } from 'react-native';
-import Gradient from './assets/Icons/Gradient';
-import DocumentData from './assets/Icons/DocumentData';
-import LightBulbPerson from './assets/Icons/LightbulbPerson';
-import Rocket from './assets/Icons/Rocket';
-import Logo from './assets/Icons/Logo';
-import ListCards from './components/ListCards';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Product from './components/Product';
+import { config } from "@gluestack-ui/config";
+import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { ScrollView } from "react-native";
+import Gradient from "./assets/Icons/Gradient";
+import DocumentData from "./assets/Icons/DocumentData";
+import LightBulbPerson from "./assets/Icons/LightbulbPerson";
+import Rocket from "./assets/Icons/Rocket";
+import Logo from "./assets/Icons/Logo";
+// import ListCards from "./components/ListCards";
+import Login from "./presentation/components/Login";
+import Register from "./presentation/components/Register";
+import Home from "./presentation/screens/HomeScreen";
+import Product from "./presentation/screens/ProductScreen";
 
 export default function App() {
-  return (
-    <GluestackUIProvider config={config}>
-      {/*<Home />*/}
-      {<Product/>}
-      {/* <Login/>  */}
-      {/* <Register/> */}
-      {/* <Catalogo/> */}
-      {/* <Prueba/>  */}
-      {/* <Profile/> */}
-      {/* <Inicio/> */}
-    </GluestackUIProvider>
-  );
+    return (
+        <GluestackUIProvider config={config}>
+            {/*<Home />*/}
+            {<Product />}
+            {/* <Login/>  */}
+            {/* <Register/> */}
+            {/* <Catalogo/> */}
+            {/* <Prueba/>  */}
+            {/* <Profile/> */}
+            {/* <Inicio/> */}
+        </GluestackUIProvider>
+    );
 }
 
 /*const Home = () => {
