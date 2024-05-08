11145fd03c4ee584c5b0c4e1cec2b67e44f44551
JoahanOrtega <joahangab23@gmail.com>
2024-05-07 22:00:28 -0600

    Update App.tsx

--- shaba/App.tsx
+++ shaba/App.tsx
@@ -1,30 +1,30 @@
-import { config } from '@gluestack-ui/config';
-import { Box, GluestackUIProvider, Text } from '@gluestack-ui/themed';
-import { ScrollView } from 'react-native';
-import Gradient from './assets/Icons/Gradient';
-import DocumentData from './assets/Icons/DocumentData';
-import LightBulbPerson from './assets/Icons/LightbulbPerson';
-import Rocket from './assets/Icons/Rocket';
-import Logo from './assets/Icons/Logo';
+import { config } from "@gluestack-ui/config";
+import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { ScrollView } from "react-native";
+import Gradient from "./assets/Icons/Gradient";
+import DocumentData from "./assets/Icons/DocumentData";
+import LightBulbPerson from "./assets/Icons/LightbulbPerson";
+import Rocket from "./assets/Icons/Rocket";
+import Logo from "./assets/Icons/Logo";
 //import Login from './components/Login';
 //import Register from './components/Register';
-import {HomeScreen} from './presentation/screens/HomeScreen';
+import { HomeScreen } from "./presentation/screens/HomeScreen";
 //import Profile from './components/Profile';
-import Profile from './presentation/screens/Profile';
+import Profile from "./presentation/screens/Profile";
 
 export default function App() {
-  return (
-    <GluestackUIProvider config={config}>
-      {/*<HomeScreen/>*/}
-      {/*<Product/>*/}
-      {/* <Login/>  */}
-      {/* <Register/> */}
-      {/* <Catalogo/> */}
-      {/* <Prueba/>  */}
-      {<Profile/>}
-      {/* <Inicio/> */}
-    </GluestackUIProvider>
-  );
+    return (
+        <GluestackUIProvider config={config}>
+            <HomeScreen />
+            {/*<Product/>*/}
+            {/* <Login/>  */}
+            {/* <Register/> */}
+            {/* <Catalogo/> */}
+            {/* <Prueba/>  */}
+            {/* {<Profile/>} */}
+            {/* <Inicio/> */}
+        </GluestackUIProvider>
+    );
 }
 
 /*const Home = () => {
